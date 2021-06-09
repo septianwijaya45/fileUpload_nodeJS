a195aa591ff30d4412e377c6b4a31309ca09a61b
Septian Wijaya Aminulloh <septianwijaya170845@gmail.com>
2021-06-09 20:07:11 +0700

    Setting Database

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+// dotenv
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parsing application/x-www-form-urlcode
+app.use(express.static('public'));
+
+// Templating
+app.engine(
+    'hbs',
+    exphbs({
+        extname: '.hbs',
+        defaultLayout: 'main',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: path.join(__dirname, '/views/layouts/partials')
+    })
+)
+app.set('view engine', 'hbs');
+
+// database setting
+const db = require('./db_config');
+
+// ROUTING
+
+// END OF ROUTING
+
+app.listen(port, () =>{
+    console.log(`Server is running with port ${port}`);
+})
---
+++ db_config.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : process.env.DB_HOST,
+    database        : process.env.DB_NAME,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS
+});
+
+module.exports = {pool};

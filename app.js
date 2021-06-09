27e1468619a8c8c398eb0e617bdad98b23c320a7
Septian Wijaya Aminulloh <septianwijaya170845@gmail.com>
2021-06-10 03:23:43 +0700

    Declarate FileUpload & views

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 // dotenv
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// File Upload
+app.use(fileUpload());
 
 // Parsing application/x-www-form-urlcode
 app.use(express.static('public'));
